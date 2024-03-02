0b8ff482f30179375a5cf7d41a4abbecdd6e8f26
Nut Suebthawinkul <smt.nuttasit@gmail.com>
2024-03-02 15:03:11 -0500

    haveanaccount?button

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -38,6 +38,9 @@ const Login = () => {
 
           {action==="Sign Up"?<div></div>: <div className="forget-password">Lost Password?  <a href="https://www.google.com/"> Click here!</a></div>}
 
+          {action === "Login" ? (<div></div>) : ( <div className="forget-password">
+            Have an account? <a onClick={() => setAction("Login")}> Click here!</a></div>)}
+
           <div className="submit-container">
             <div className={action=="Login"?"submit gray": "submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
             <div className={action=="Sign Up"?"submit gray": "submit"} onClick={()=>{setAction("Login")}}>Login</div>

207cfebd7f978f09a30d7199721f4ef505599e13
Jason Ye <jasonye0104@gmail.com>
2024-03-02 12:06:11 -0500

    added side menu bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'r
 import Home from './pages/home/home';
 import Login from './pages/login/login'
 import Signup from './pages/signup/signup';
+import Dashboard from './pages/dashboard/dashboard';
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function RoutesContent() {
         <Route path='/' element={<Home />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
-
+        <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
   );
 }
---
+++ src/components/sidemenu/SideMenu.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Drawer, Button, List, ListItem, ListItemText } from '@mui/material';
+
+const SideMenu = () => {
+  const [state, setState] = useState({
+    left: false,
+    right: false,
+    top: false,
+    bottom: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Option 1', 'Option 2', 'Option 3'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div>
+      {(['left']).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+};
+
+export default SideMenu;

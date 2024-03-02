711d12ce492be462c1dc2ac3b906058bb75659d3
Nut Suebthawinkul <smt.nuttasit@gmail.com>
2024-03-02 17:09:47 -0500

    gobackkk

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -89,73 +89,83 @@ const Signup = () => {
   };
 
   return (
-    <Container sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <Box sx={{ width: '100%', maxWidth: 450, textAlign: 'center' }}>
-        <Typography variant="h4" sx={{ mb: 2 }}>Sign Up</Typography>
-        <Box component="form" onSubmit={handleSignup} noValidate>
-          <TextField
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            required
-            label="First Name"
-            type="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <TextField
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            required
-            label="Last Name"
-            type="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <TextField
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            required
-            label="Email"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            required
-            label="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-            Sign Up
-          </Button>
-
-          <Button fullWidth variant="outlined" sx={{ mt: 1, mb: 2 }} onClick={signInWithGoogle}>
-            Sign up with Google
-          </Button>
-
-          <Grid container>
-            <Grid item xs>
-              <Link href='\login' variant="body2">
-                Already have an account?
-              </Link>
-            </Grid>
+    <Container>
+      <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Grid container>
+          <Grid item xs>
+            <Link href='/' variant="body10">
+              Go back 
+            </Link>
           </Grid>
-        </Box>
+        </Grid>
       </Box>
-      <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-        <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
+      <Container sx={{ marginTop: -7, display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+        <Box sx={{ width: '100%', maxWidth: 450, textAlign: 'center' }}>
+          <Typography variant="h4" sx={{ mb: 2 }}>Sign Up</Typography>         
+          <Box component="form" onSubmit={handleSignup} noValidate>
+            <TextField
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              required
+              label="First Name"
+              type="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              required
+              label="Last Name"
+              type="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              required
+              label="Email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              fullWidth
+              variant="outlined"
+              margin="normal"
+              required
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign Up
+            </Button>
 
+            <Button fullWidth variant="outlined" sx={{ mt: 1, mb: 2 }} onClick={signInWithGoogle}>
+              Sign up with Google
+            </Button>
+
+            <Grid container>
+              <Grid item xs>
+                <Link href='\login' variant="body2">
+                  Already have an account?
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+          <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
+        </Container>
     </Container>
   );
 };

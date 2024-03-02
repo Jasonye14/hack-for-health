3a54e3f601ec9789b70b3618d7dce5061f0c4766
Jason Ye <jasonye0104@gmail.com>
2024-03-02 13:45:27 -0500

    stored settings page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login/login'
 import Signup from './pages/signup/signup';
 import Dashboard from './pages/dashboard/dashboard';
 import ChatBotPage from './pages/Gemini/chatbot';
+import Settings from './pages/settings/settings';
+import Checker from './pages/compatability-checker/CompatabilityCheck';
 
 function App() {
   return (
@@ -22,10 +24,11 @@ function RoutesContent() {
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/compatability-checker' element={<></>} />
+        <Route path='/compatability-checker' element={<Checker/>} />
         <Route path='/gemini-chat-bot' element={<ChatBotPage/>} />
         <Route path='/history' element={<></>} />
         <Route path='/profile' element={<></>} />
+        <Route path='/settings' element={<Settings/>} />
         <Route path='/my-prescriptions' element={<Dashboard />} />
         <Route path="*" element={<></>} />
       </Routes>

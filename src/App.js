1a1c3929710e84aee28ecae7e6e9e9f99c2284c5
Jason Ye <jasonye0104@gmail.com>
2024-03-02 21:47:41 -0500

    protected route added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/login/login'
 import Signup from './pages/signup/signup';
@@ -9,6 +9,8 @@ import ChatBotPage from './pages/Gemini/chatbot';
 import Settings from './pages/settings/settings';
 import Checker from './pages/compatability-checker/CompatabilityCheck';
 import History from './pages/history/history';
+import NotFound from './pages/NotFound/notfound';
+import ProtectedRoute from './pages/protectedRoute/ProtectedRoute';
 
 function App() {
   return (
@@ -19,20 +21,19 @@ function App() {
 }
 function RoutesContent() {
 
-  return(
+  return (
     <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/compatability-checker' element={<Checker/>} />
-        <Route path='/gemini-chat-bot' element={<ChatBotPage/>} />
-        <Route path='/history' element={<History/>} />
-        <Route path='/profile' element={<></>} />
-        <Route path='/settings' element={<Settings/>} />
-        <Route path='/my-prescriptions' element={<Dashboard />} />
-        <Route path="*" element={<></>} />
-      </Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/dashboard' element={<ProtectedRoute>  <Dashboard/> </ProtectedRoute>} />
+      <Route path='/compatability-checker' element={<ProtectedRoute>  <Checker/> </ProtectedRoute>} />
+      <Route path='/gemini-chat-bot' element={<ProtectedRoute>  <ChatBotPage/> </ProtectedRoute>} />
+      <Route path='/history' element={<ProtectedRoute > <History/> </ProtectedRoute>} />
+      <Route path='/settings' element={<ProtectedRoute> <Settings/>  </ProtectedRoute>} />
+      <Route path='/my-prescriptions' element={<ProtectedRoute><Dashboard/></ProtectedRoute>} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 export default App;
---
+++ src/pages/NotFound/notfound.js
@@ -0,0 +1,13 @@
+// In src/pages/NotFound/NotFound.js
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for doesn't exist or another error occurred.</p>
+    </div>
+  );
+}
+
+export default NotFound;
